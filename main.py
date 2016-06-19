81a1b7f3dc1bf1e291dbc107755a5d8913471269
Aditya Jain <96adityajain@gmail.com>
2016-06-20 01:05:09 +0530

    returning json object of output

--- main.py
+++ main.py
@@ -38,11 +38,22 @@ print("\n\nUser Handle:" + string_query)
 print("Total rated contests participated:\t" + str(no_of_contest))
 print("\n")
 
-print("oldRating\tnewRating")
-for i in data:
-	print (str(i['oldRating']) + "\t\t" + str(i['newRating']))
-#	print i['newRating']
 
+adi={}
+adi['handle']=string_query
+
+cont=[]
+for i in data:
+	j={}
+	j['Contest Name']=i['contestName']
+	j['Contest ID']=i['contestId']
+	j['Old Rating']=i['oldRating']
+	j['New Rating']=i['newRating']
+	cont.append(j)
+
+adi['contests']=cont
+
+adi=json.dumps(adi)
+print(adi)
 
 exit()
-
